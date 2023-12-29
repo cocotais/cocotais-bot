6b5fc92133ac3ef72a5d2be1c9caed3b35291dff
刘lyxAndy <liuyuxuanandy@outlook.com>
2023-12-29 20:57:54 +0800

    💩&🚧 尝试使用官方SDK

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import fse from 'fs-extra'
 import { startBot } from './start'
-
+import { GetWsParam, Config } from 'qq-guild-bot';
 
 if (fse.existsSync('./config.json')) {
     fse.readJson('./config.json')
-        .then(async (value: any) => {
+        .then(async (value: GetWsParam & Config) => {
             await startBot(value)
         })
         .catch((err) => {
@@ -13,10 +13,9 @@ if (fse.existsSync('./config.json')) {
 } else {
     console.log("您还没有生成配置文件，已为您自动生成。")
     fse.writeFileSync("./config.json", `{
-    "id": "",
-    "secret": "",
+    "appID": "",
     "token": "",
-    "type": "private"
+    "intents": []
 }`)
     process.exit()
 }
--- src/start.ts
+++ src/start.ts
@@ -1,3 +1,14 @@
-export async function startBot(options: any) {
+import { GetWsParam, Config, createOpenAPI, createWebsocket } from 'qq-guild-bot';
+import { botHandler } from './bot';
 
+export async function startBot(options: GetWsParam & Config) {
+    const testConfig = options;
+
+    // 创建 client
+    const client = createOpenAPI(testConfig);
+
+    // 创建 websocket 连接
+    const ws = createWebsocket(testConfig);
+
+    botHandler(client,ws)
 }
