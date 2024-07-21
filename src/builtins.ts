09068f98b3a8c4e85b1c461987465cdca759938d
刘lyxAndy <liuyuxuanandy@outlook.com>
2024-07-21 19:42:16 +0800

    ✨ echo内置插件

--- src/builtins.ts
+++ src/builtins.ts
@@ -47,6 +47,48 @@ export function getBuiltinPlugins(){
                 })
             })
             return plugin
+        }(),
+        function(){
+            const plugin = new CocotaisBotPlugin("builtin:echo","1.0.0")
+            plugin.onMounted((bot) => {
+                plugin.command.register("/echo","重复你说的话", (type, _msgs, event) => {
+                    let content = _msgs.join(" ")
+                    if(type == 'c2c'){
+                        let uid = (event as C2cMessageEvent).user.id
+                        let mid = (event as C2cMessageEvent).id
+                        bot.c2cApi.postMessage(uid, {
+                            msg_type: 0,
+                            content: content,
+                            msg_id: mid
+                        })
+                    }
+                    else if(type == 'group'){
+                        let gid = (event as GroupMessageEvent).group.id
+                        let mid = (event as GroupMessageEvent).id
+                        bot.groupApi.postMessage(gid, {
+                            msg_type: 0,
+                            content: content,
+                            msg_id: mid
+                        })
+                    }
+                    else if(type == 'guild'){
+                        let cid = (event as GuildMessageEvent).channel.id
+                        let mid = (event as GuildMessageEvent).id
+                        bot.messageApi.postMessage(cid, {
+                            content: content,
+                            msg_id: mid
+                        })
+                    }
+                    else if(type == 'direct'){
+                        let gid = (event as GuildMessageEvent).guild.id
+                        bot.directMessageApi.postDirectMessage(gid, {
+                            content: content,
+                            msg_id: (event as GuildMessageEvent).id
+                        })
+                    }
+                })
+            })
+            return plugin
         }()
     ]
 }
